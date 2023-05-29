6745cbb2133d9c8b5c7a8a81db5ebea27b79b21a
SimoneFerrini <ferrini.simone4@gmail.com>
2023-05-30 01:02:55 +0200

    ended milestone 4- started milestone 5

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,41 @@
-<script >
+<script>
+import axios from 'axios';
+import ProjectCard from './components/ProjectCard.vue';
+
+export default{
+  name: 'App',
+
+  data(){
+    return{
+      projects: [],
+    }
+  },
+
+  components:{
+    ProjectCard,
+  },  
+
+  created() {
+    this.getProjects();
+  },
+
+  methods:{
+    getProjects(){
+      axios.get('http://127.0.0.1:8000/api/projects').then(res =>{
+        this.projects = res.data.results;
+      });
+    }
+  },
+}
 
 </script>
 
 <template>
-  <h1 class="text-center">ciao birillo</h1>
+  <h1 class="text-center">I miei progetti: </h1>
+  <div class="text-center w-75 mx-auto">
+    
+  </div>
+  
 </template>
 
 <style scoped lang="scss">
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,52 @@
+<script>
+export default {
+    name: 'ProjectCard',
+
+    data() {
+        return {
+            imgUrl: 'http://127.0.0.1:8000',
+        }
+    },
+
+    props: {
+        project: Object,
+    },
+
+    computed: {
+
+        shortContent() {
+            if (this.project.description.length > 30) {
+                return this.project.description.substring(0, 30) + '...';
+            } else {
+                return this.project.description;
+            }
+        },
+
+        cover() {
+            if (this.project.cover_image == null) {
+                return 'https://as1.ftcdn.net/v2/jpg/02/99/61/74/1000_F_299617487_fPJ8v9Onthhzwnp4ftILrtSGKs1JCrbh.jpg';
+            } else {
+                return this.imgUrl + 'storage/' + this.project.cover_image;
+            }
+        },
+    }
+}
+</script>
+
+<template>
+    <div class="card" style="width: 18rem;">
+        <img :src="cover()" class="card-img-top" alt="img">
+        <div class="card-body">
+            <h5 class="card-title">{{ project.title }}</h5>
+            <p class="card-text">{{ shortContent() }}</p>
+            <small>Type: {{ project.type }}</small>
+            <div class="d-flex justify-content-between overflow-x-auto">
+                <span class="badge rounded-pill bg-primary" vfor="tecnology in project.technologies">
+                    {{ technology.name }}
+                </span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss"></style>
